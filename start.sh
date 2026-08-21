71b4133231eff5cf7d1d2175083c93ea60e7d9c2
VinceToni <vincentoni361@gmail.com>
2026-08-21 20:09:41 +0100

    fix naming

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-python bot/telegram/bot.py &
-python bot/discord/bot.py &
+
+# Export root folder so Python finds 'database' and other modules
+export PYTHONPATH=.
+
+# Start Telegram Bot in the background
+python bots/telegram/bot.py &
+
+# Start Discord Bot in the background
+python bots/discord/bot.py &
+
+# Start FastAPI server in the foreground
 uvicorn main:app --host 0.0.0.0 --port $PORT
